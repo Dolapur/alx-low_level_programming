681e6e52ce4418fbd4235f7adc0a3936286eda4d
Dolapo Adegoju <tichiegoju@gmail.com>
2022-09-13 10:02:57 -0700

    Nested loop

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _islower -> checks if the character is lowercase
+ *
+ * @c: a charcter argument
+ *
+ * Return: returns 1 and 0 depending on condition
+*/
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z' c++)
+	{
+		return (1);
+	}
+	else
+		return (0);
+}
