673c8aec3536fc98c520a3280fb4938121e1420a
Dolapo Adegoju <tichiegoju@gmail.com>
2022-09-13 07:55:33 -0700

    Using a prototype function

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the lowercase alphabets
+*/
+void print_alphabet(void)
+{
+	int b;
+
+	for (b = 'a'; b <= 'z'; b++)
+	{
+	_putchar(b);
+	}
+	_putchar('\n');
+}
