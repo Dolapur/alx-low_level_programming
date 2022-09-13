7e8384bf87732202105d4b33faadcf93af90d43c
Dolapo Adegoju <tichiegoju@gmail.com>
2022-09-13 09:23:29 -0700

    Nested loop

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 -> prints the lowercase alphabets
+*/
+void print_alphabet_x10(void)
+{
+	int x;
+	int b;
+
+	for (x = '0'; x <= '10'; x++)
+	{
+		for (b = 'a'; b <= 'z'; b++)
+		{
+			_putchar(b);
+		}
+		_putchar('\n');
+	}
+}
