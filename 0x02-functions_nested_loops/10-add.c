01a4fef508e56ab8bf2f500392e44a1eda44a068
Dolapo Adegoju <tichiegoju@gmail.com>
2022-09-13 13:40:55 -0700

    Prototype Functions

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - Adds two integers
+ *
+ * @a: parameter one
+ *
+ * @b: parameter two
+ *
+ * Return: The result of the addition.
+*/
+int add(int a, int b)
+{
+	return (a + b);
+}
