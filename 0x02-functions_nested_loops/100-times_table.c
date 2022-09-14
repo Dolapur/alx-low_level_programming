e6dfb82d07ed7883e17cb3fbcd7c0247f91f7d6e
Dolapo Adegoju <tichiegoju@gmail.com>
2022-09-13 20:16:45 -0700

    fibonacci  series

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -13,32 +13,39 @@ void print_times_table(int n)
 	{
 		for (a = 0; a <= n; a++)
 		{
-			_putchar('0');
-
 			for (b = 1; b <= n; b++)
 			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
-
-			c = a * b;
-
-			if (c <= 99)
-				_putchar(' ');
-			else if (c <= 9)
-				_putchar(' ');
-			else if (c >= 100)
-			{
-				_putchar((c / 100) + '0');
-				_putchar(((c / 10)) % 10 + '0');
+				c = a * b;
+				if (b == 0)
+				{
+					_putchar(c + '0');
+				}
+				else if (c < 10 && b != 0)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(c + '0');
+				}
+				else if (c >= 10 && c < 100)
+					{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar((c / 10) + '0');
+					_putchar((c % 10) + '0');
+					}
+				else if (c >= 100)
+					{
+					_putchar(',');
+					_putchar(' ');
+					_putchar((c / 100) + '0');
+					_putchar(((c / 10) % 10) + '0');
+					_putchar((c % 10) + '0');
+					}
 			}
-			else if (c <= 99 && c >= 10)
-			{
-				_putchar((c / 10) + '0');
-			}
-			_putchar((c % 10) + '0');
-			}
-		_putchar('\n');
+			_putchar('\n');
 		}
 	}
 }
--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -7,22 +7,23 @@
 */
 int main(void)
 {
-	int count;
+	int i;
 
-	unsigned long fib1 = 0, fib2 = 1, sum;
+	unsigned long int j, k, next, sum;
 
-	for (count = 0; count < 50; count++)
+	j = 1;
+	k = 2;
+	sum = 0;
+	for (i = 1; i <= 33; ++i)
 	{
-		sum = fib1 + fib2;
-
-		printf("%lu", sum);
-		fib1 = fib2;
-
-		fib2 = sum;
-		if (count == 49)
-		printf("\n");
-
-		else
-			printf(", ");
+		if (j < 4000000 && (j % 2) == 0)
+		{
+			sum = sum + j;
+		}
+		next = j + k;
+		j = k;
+		k = next;
 	}
+	printf("%lu\n", sum);
+	return (0);
 }
