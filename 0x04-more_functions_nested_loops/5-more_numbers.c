875c676ef406e9c03b40f178365e6ef4d16a3c7c
Dolapo Adegoju <tichiegoju@gmail.com>
2022-09-15 06:25:40 -0700

    Prototype function

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,11 +7,11 @@ void more_numbers(void)
 {
 	int count, num;
 
-	for (count = 0; count < 10; count++)
+	for (count = 0; count <= 9; count++)
 	{
 		for (num = 0; num <= 14; num++)
 		{
-			if (num > 10)
+			if (num > 9)
 				_putchar((num % 10) + '0');
 				_putchar((num / 10) + '0');
 		}
