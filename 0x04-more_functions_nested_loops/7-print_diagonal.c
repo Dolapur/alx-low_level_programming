60a28141eaa280224ee1e48488c5ff9baa8428e2
Dolapo Adegoju <tichiegoju@gmail.com>
2022-09-15 07:34:59 -0700

    Prototype functions

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -7,21 +7,14 @@
 */
 void print_diagonal(int n)
 {
-	int s, t;
+	int t;
 
-	for (s = 0; s < n; s++)
+	for (t = 0; t < n; t++)
 	{
-		for (t = 0; t < n; t++)
+		if (n > 0)
 		{
-			if (n > 0)
-			{
-				_putchar(' ');
-				_putchar('\\');
-			}
-			if  (n == -1)
-				continue;
+			_putchar('\\');
 		}
-		_putchar('\n');
 	}
 	_putchar('\n');
 }
