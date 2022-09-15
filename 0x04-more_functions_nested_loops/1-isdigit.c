e9fce9d5a6a0af3d0b666068587585dda7fa219e
Dolapo Adegoju <tichiegoju@gmail.com>
2022-09-15 05:20:44 -0700

    Understanding nested loop better

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _isdigit - Checks for a digit (0-9)
+ *
+ * @c: print int
+ *
+ * Return: 0 or 1 depending on the condition
+*/
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
