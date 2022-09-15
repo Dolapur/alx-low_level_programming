7254cd66e324cf120f3c8c2429f60c1297ef5b9b
Dolapo Adegoju <tichiegoju@gmail.com>
2022-09-15 05:52:30 -0700

    Prototype functions

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - Prints the numbers from 0-9 except for 2 and 4.
+*/
+void print_most_numbers(void)
+{
+	int j;
+
+	for (j = 0; j <= 9; j++)
+	{
+		if (j != 2 && j != 4)
+		{
+			_putchar((j % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
