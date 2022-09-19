db5beae6eb0f27e613e3691f5a79536bef48e057
Dolapo Adegoju <tichiegoju@gmail.com>
2022-09-19 13:54:58 -0700

    C pointers

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * puts_half - prints every other character of a string
+ * @str: A pointer to an int that will be changed
+*/
+void puts_half(char *str)
+{
+	int a, b;
+
+	while (str[a] != '\0')
+	{
+		a++;
+	}
+	b = (a + 1) / 2;
+
+	for (a = b; str[a]; a++)
+	{
+	_putchar (str[a]);
+	}
+_putchar ('\n');
+}
---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array - prints n elements of an array of integers
+ * @a: A pointer to an int that will be updated
+ * @n: The number of elements to be printed
+*/
+void print_array(int *a, int n)
+{
+	int i;
+
+	i = 0;
+	while (i < n)
+	{
+		printf("%d", a[i]);
+	}
+	if (i < n - 1)
+	{
+		printf(",");
+	}
+	i++;
+}
