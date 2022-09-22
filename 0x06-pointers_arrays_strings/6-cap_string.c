1c4a8dd6a7e78dac7bd9f87c442c96c6a5aedf2a
Dolapo Adegoju <tichiegoju@gmail.com>
2022-09-21 19:26:37 -0700

    task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -9,7 +9,7 @@ char *cap_string(char *str)
 {
 	int i = 0;
 
-	while (str[++i])
+	while (str[i])
 	{
 
 	while (!(str[i] >= 'a' && str[i] <= 'z'))
@@ -30,6 +30,7 @@ char *cap_string(char *str)
 				str[i - 1] == '}' ||
 				i == 0)
 			str[i] = str[i] - 32;
+		i++
 	}
 	return (str);
 }
