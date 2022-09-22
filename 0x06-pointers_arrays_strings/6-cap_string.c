6dd89833362760ea5fea4e656cf958c26a8caad9
Dolapo Adegoju <tichiegoju@gmail.com>
2022-09-21 18:48:34 -0700

    Cap function

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -9,8 +9,13 @@ char *cap_string(char *str)
 {
 	int i = 0;
 
-	for (str[i] != '\0'; str[i] >= 'a' && str[i] <= 'z'; i++)
+	while (str[i] = '\0')
 	{
+
+	while (!(str[i] >= 'a' && str[i] <= 'z'))
+	{
+		i++;
+
 		if (str[i - 1] == ' ' ||
 				str[i - 1] == '\t' ||
 				str[i - 1] == '\n' ||
@@ -25,9 +30,9 @@ char *cap_string(char *str)
 				str[i - 1] == '{' ||
 				str[i - 1] == '}' ||
 				i == 0)
-		{
 			str[i] -= 32;
-		}
+	}
+	i++;
 	}
 	return (str);
 }
