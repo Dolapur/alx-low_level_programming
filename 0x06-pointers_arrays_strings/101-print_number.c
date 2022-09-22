a36253e104c5dbfb874b942ca5424dd4097cc862
Dolapo Adegoju <tichiegoju@gmail.com>
2022-09-22 09:27:00 -0700

    Prints an integer using putchar function

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -8,18 +8,13 @@ void print_number(int n)
 {
 	unsigned int num = n;
 
-	if (num < 0)
+	if (n < 0)
 	{
 		_putchar('-');
 		num = -num;
 	}
-	else if (n == 0)
-	{
-		_putchar('0');
-	}
-	else if ((num / 10) > 0)
-	{
-		_putchar(num / 10);
-	}
-	 _putchar((num % 10) + '0');
+	if ((num / 10) > 0)
+		print_number(num / 10);
+
+	_putchar((num % 10) + '0');
 }
