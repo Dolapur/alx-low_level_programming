b0559dff8bebe44acfbd56fbe731a7450571e754
Dolapo Adegoju <tichiegoju@gmail.com>
2022-09-26 10:27:39 -0700

    Using the strstr function

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strstr - first occurrence of the substring needle in the string haystack
+ * @haystack: main str to be examined
+ * @needle: The substring to be located.
+ * Return: return 0
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int i;
+
+	if (*needle == 0)
+		return (haystack);
+	while (*haystack)
+	{
+		i = 0;
+
+		if (haystack[i] == needle[i])
+		{
+			do {
+				if (needle[i + 1] == '\0')
+					return (haystack);
+			} while (haystack[i] = needle[i]);
+		}
+		haystack++;
+	}
+	return (0);
+}
