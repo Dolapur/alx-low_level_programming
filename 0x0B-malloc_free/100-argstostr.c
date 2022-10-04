b4a1342efe5355c8ee0c7b6146c0350daa513e64
Dolapo Adegoju <tichiegoju@gmail.com>
2022-10-04 06:53:54 -0700

    Using argstostr function

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *  argstostr - concatenates all arguments of a program
+ *  @ac: first parameter integer
+ *  @av: second parameter pointer to a pointer
+ *  Return: NULL if ac=0 or av=NULL
+ */
+char *argstostr(int ac, char **av)
+{
+	char *str;
+	int a, b, c = 0;
+	int size;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+	for (a = 0; a < ac; a++)
+	{
+		for (b = 0; av[a][b]; b++)
+			size++;
+	}
+	str = malloc(sizeof(char) * (size + 1));
+
+	for (a = 0; a < ac; a++)
+	{
+		for (b = 0; av[a][b]; b++)
+		{
+		str[c++] = av[a][b];
+		}
+		str[c++] = '\n';
+	}
+	str[size] = '\0';
+	return (str);
+}
