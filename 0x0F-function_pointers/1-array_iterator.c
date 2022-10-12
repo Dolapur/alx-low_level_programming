9de254567781cd8460af1c72855596ec4e52dfeb
Dolapo Adegoju <tichiegoju@gmail.com>
2022-10-12 08:39:42 -0700

    function pointers project

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -9,11 +9,13 @@
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	unsigned int i;
 
-	if (array && action)
+	if (array != NULL && action != NULL)
 	{
-		for (i = 0; i < size; i++)
-			action = array[i];
+		while (size-- > 0)
+		{
+			action(*array);
+			array++;
+		}
 	}
 }
