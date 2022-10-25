137025e77092635e2317af97e6c704f64c757221
Dolapo Adegoju <tichiegoju@gmail.com>
2022-10-25 07:21:54 -0700

    Completed

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -13,17 +13,19 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	listint_t *tmp, *copy = *head;
 	unsigned int node;
 
-	if (copy == NULL)		
+	if (copy == NULL)
 		return (-1);
 	if (index == 0)
 	{
-		*head = (*head)->next;								free(copy);
+		*head = (*head)->next;
+		free(copy);
 		return (1);
 	}
 	for (node = 0; node < (index - 1); node++)
 	{
 		if (copy->next == NULL)
-			return (-1);								copy = copy->next;
+			return (-1);
+		copy = copy->next;
 	}
 	tmp = copy->next;
 	copy->next = tmp->next;
