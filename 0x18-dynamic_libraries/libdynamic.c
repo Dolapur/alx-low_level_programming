51756eec48f1aa3bf1892d6d27d85032bbe15945
Dolapo Adegoju <tichiegoju@gmail.com>
2022-12-12 17:59:23 -0800

    fixed

--- 0x18-dynamic_libraries/libdynamic.c
+++ 0x18-dynamic_libraries/libdynamic.c
@@ -1,12 +1,250 @@
-#include <stdio.h>
 #include "main.h"
+
 /**
- *  * main - check the code for Holberton School students.
+ * _abs - Computes the absolute value of an integer
+ * @n: parameter
  *
- *    * Return: Always EXIT_SUCCESS.
+ * Return: The absolute value of the integer
  */
-int main(void)
+int _abs(int n)
 {
-	printf("%d\n", _strlen("Dolapo"));
-	return (EXIT_SUCCESS);
+	if (n <= 0)
+		return (-n);
+	else
+		return (n);
 }
+
+int _atoi(char *s)
+{
+	int sign = 1;
+	unsigned int num = 0;
+
+	do {
+		if (*s == '-')
+			sign *= -1;
+		else if (*s >= '0' && *s <= '9')
+			num = (num * 10) + (*s - '0');
+		else if (num > 0)
+			break;
+	} while (*s++);
+	return (num * sign);
+}
+
+int _isalpha(int c)                                                                       
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
+	else
+		return (0);
+}
+
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+	else
+		return (0);
+}
+
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}
+
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
+}
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		dest[i] = src[i];
+	return (dest);
+}
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		s[i] = b;
+	return (s);
+}
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
+void _puts(char *str)
+{
+	while (*str)
+		_putchar(*str++);
+	_putchar('\n');
+}
+
+char *_strcat(char *dest, char *src)
+{
+	int a = 0;
+	int b = 0;
+
+	while (dest[a] != '\0')
+		a++;
+	while (src[b] != '\0')
+	{
+		dest[a] = src[b];
+		a++;
+		b++;
+	}
+	dest[a] = '\0';
+	return (dest);
+}
+
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; s[i] >= '\0'; i++)
+	{
+		if (s[i] == c)
+			return (s + i);
+	}
+	return (0);
+}
+
+int _strcmp(char *s1, char *s2)
+{
+	char *a = s1;
+	char *b = s2;
+
+	while (*a != '\0' && *b != '\0' && *a == *b)
+	{
+		a++;
+		b++;
+	}
+	return (*a - *b);
+}
+
+char *_strcpy(char *dest, char *src)
+{
+	int q = 0;
+
+	while (src[q] != '\0')
+	{
+		dest[q] = src[q];
+		q++;
+	}
+	dest[q] = '\0';
+	return (dest);
+}
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (s[len] != '\0')
+		len++;
+	return (len);
+}
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int c = 0;
+	int f = 0;
+
+	while (dest[c] != '\0')
+		c++;
+	while (f < n && src[f] != '\0')
+	{
+		dest[c] = src[f];
+		c++;
+	}
+	dest[c] = '\0';
+	return (dest);
+}
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+
+	while (src[i] && i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+	return (dest);
+}
+
+char *_strpbrk(char *s, char *accept)
+{
+	int i;
+
+	while (*s)
+	{
+		for (i = 0; accept[i]; i++)
+		{
+			if (*s == accept[i])
+				return (s);
+		}
+		s++;
+	}
+	return (0);
+}
+
+unsigned int _strspn(char *s, char *accept)
+{
+	int i;
+	unsigned int bytes = 0;
+
+	while (*s)
+	{
+		for (i = 0; accept[i]; i++)
+		{
+			if (*s == accept[i])
+			{
+				bytes++;
+				break;
+			}
+			else if (accept[i + 1] == '\0')
+				return (bytes);
+		}
+		s++;
+	}
+	return (bytes);
+}
+
+char *_strstr(char *haystack, char *needle)
+{
+	char *s1, *s2;
+	
+	while (*haystack != '\0')
+	{
+		s1 = haystack;
+		s2 = needle;
+
+		while (*haystack != '\0' && *s2 != '\0' && *haystack == *s2)
+		{
+			haystack++;
+			s2++;
+		}
+		if (*s2 == '\0')
+			return (s1);
+		haystack = s1 + 1;
+	}
+	return (0);
+}
+
